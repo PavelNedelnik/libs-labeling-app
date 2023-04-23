629765290f2343b46d63be5a3073bb82f2df2200
nedelnik <p.nedelnik@email.cz>
2023-04-23 17:39:20 +0200

    demo front end updates

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+from matplotlib import cm
+
+DIM = (70, 70) # hyperspectral map dimensions
+CLASS_COLORS = cm.Set1
+INTENSITY_COLORS = cm.Reds
+RANGE_SLIDER_COLORS = ['red']
+
+GRAPH_STYLE = dict(
+    template='plotly_dark',
+    plot_bgcolor= 'rgba(0, 0, 0, 0)',
+    paper_bgcolor= 'rgba(0, 0, 0, 0)',
+    margin=dict(l=0, r=0, b=0, t=0,),
+)
