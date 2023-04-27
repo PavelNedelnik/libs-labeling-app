2d80dcc41fc592c77434b6a39458d510392fafb7
nedelnik <p.nedelnik@email.cz>
2023-04-27 12:40:32 +0200

    changed to light theme

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,12 @@ CLASS_COLORS = cm.Set1
 INTENSITY_COLORS = cm.Reds
 RANGE_SLIDER_COLORS = ['red']
 
-GRAPH_STYLE = dict(
+GRAPH_STYLE = dict()
+"""
+dict(
     template='plotly_dark',
     plot_bgcolor= 'rgba(0, 0, 0, 0)',
     paper_bgcolor= 'rgba(0, 0, 0, 0)',
     margin=dict(l=0, r=0, b=0, t=0,),
 )
+"""
