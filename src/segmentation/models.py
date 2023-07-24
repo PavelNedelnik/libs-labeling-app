8e5875c5d3aeefad94afb86e91e82d60918de834
Pavel Nedelnik <p.nedelnik@email.cz>
2023-07-24 17:27:52 +0200

    removed unused model

--- src/segmentation/models.py
+++ src/segmentation/models.py
@@ -14,19 +14,14 @@ from sklearn.cluster import KMeans
 filtering_predicate = lambda x: x[1] >= 0
 
 models = [KMeans(n_clusters=3, n_init='auto'),
-          #BasicWrapper(GaussianNB(), filtering_predicate),
           BasicWrapper(KNeighborsClassifier(n_jobs=-1), filtering_predicate),
           BasicWrapper(RandomForestClassifier(max_depth=3), filtering_predicate),
-          #BasicWrapper(GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0), filtering_predicate),
-          #BasicWrapper(MLPClassifier(hidden_layer_sizes=(150, 100, 50), max_iter=300, activation='relu', solver='adam', random_state=1), filtering_predicate),
-          #BasicWrapper(MLPClassifier(hidden_layer_sizes=(256, 128, 64, 32), max_iter=300, activation='relu', solver='adam', random_state=1), filtering_predicate),
-          #PCAWrapper(SVC(random_state=0), filtering_predicate),
           PCAWrapper(MLPClassifier(hidden_layer_sizes=(256, 128, 64, 32), max_iter=300, activation='relu', solver='adam', random_state=1), filtering_predicate),
-          #KerasWrapper(keras_model, filtering_predicate),
-          #TransferKerasWrapper(transfer_model, filtering_predicate, (88,44,1)),
-          #KerasWrapper(cnn_model, filtering_predicate),
           ]
 
 # maps model names to indices over <models> array
-model_names = {name: i for i, name in enumerate(
-    ['Naive KMeans', 'Bayes', 'KNN', 'Random Forest', 'Gradient Boosting', 'MLP', 'MLP_bigger', 'PCA_SVM', 'PCA_MLP', 'Keras_NN', "Transfer_model", "1D_CNN"])}
+model_names = {
+    name: i for i, name in enumerate([
+        'Naive KMeans', 'KNN', 'Random Forest', 'PCA_MLP'
+    ])
+}
